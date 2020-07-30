39519b3a7eaf20df0d7bc4506d26d7f12ae06fa1
krattz <nthebep@yahoo.com>
2020-07-30 08:41:51 +0200

    tests added for probabilities and roll method

--- src/main/java/DieTest.java
+++ src/main/java/DieTest.java
@@ -1,19 +1,50 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Random;
+
 
 public class DieTest {
-    Die die = new Die(6);
-    @Test
-    public void setProbabilities() {
+    public int value;
+    Random r = new Random();
+    private int sides;
+    private int [] probabilities;
+    double random = Math.random();
 
-        Assert.assertEquals(6, die.value);
-        Assert.assertFalse(die.random< 7);
+    @Test
+    public void setProbabilities(int [] probabilities) {
+        this.probabilities = probabilities;
+        sides = 6;
+        probabilities = new int[]{1, 1, 1, 1, 1, 6};
+        int result = 0;
+        for (int probability : probabilities) {
+            result += probability;
+        }
+
+        double [] np = new double[result];
+        if(probabilities.length ==sides){
+            for (int i = 0; i <probabilities.length ; i++) {
+                np[i] = (probabilities[i])/(result*1.0);
+            }
+            for (int i = 0; i < probabilities.length ; i++) {
+                if (random< np[i]) {
+                    value = i+1;
+                    break;
+                }
+            }
+        }
+        Assert.assertEquals(6, result);
     }
 
-    @Test
+    @org.junit.Test
     public void roll() {
+        sides = 6;
+        value = r.nextInt(sides )+1;
+        try {
+            setProbabilities(probabilities);
+        }catch (NullPointerException e){
 
-        Assert.assertFalse(die.random>6);
+        Assert.assertFalse(value ==0);
     }
+}
 }
